308b84f8a21a06012283c4df3f7d36a2ea047b87
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-04-02 14:17:22 -0700

    updated

--- app.js
+++ app.js
@@ -3,33 +3,29 @@
   var boardWidth = document.getElementById("page").offsetWidth;
   console.log("Board Width is: " + boardWidth);
   var boardHeight = document.getElementById("page").style.height = window.innerHeight;
-  console.log(screen.height);
+  console.log("boardHeight:", boardHeight);
   console.log("innerHeight is: ",  window.innerHeight)
 
+
+  window.addEventListener("resize", function() {
+    document.getElementById("page").style.height = window.innerHeight + "px";
+    document.getElementById("artboard").style.height = window.innerHeight + "px";
+  })
 
   //add divs for boardWidth/10
 
   for (var i = 0; i < boardHeight/20; i++) {
     var newLine = document.createElement("div");
-    // if (i%2 != 0) {
-    //   newLine.classList.add("line");
-    // } else {
-      newLine.classList.add("line");
-    // }
+    document.getElementById("artboard").style.height = boardHeight + "px";
+    newLine.classList.add("line");
     document.getElementById("artboard").appendChild(newLine);
-    // newLine.style.paddingBottom = 2 + "px";
     newLine.style.transitionDuration = ((Math.random() * 2.5) + .5).toString() + "s";
   }
 
-  for (var i = 0; i < boardHeight/24; i++) {
+  for (var i = 0; i < boardHeight/20; i++) {
     var newLine = document.createElement("div");
-    // if (i%2 != 0) {
-    //   newLine.classList.add("line");
-    // } else {
-      newLine.classList.add("lineTwo");
-    // }
+    newLine.classList.add("lineTwo");
     document.getElementById("artboardTwo").appendChild(newLine);
-    // newLine.style.paddingBottom = 2 + "px";
     newLine.style.transitionDuration = ((Math.random() * 2.5) + .5).toString() + "s";
   }
 
@@ -45,18 +41,22 @@
     });
   }
 
-var clickGo = function(click) {
-  document.querySelector("body").addEventListener(click, function() {
-      var intervalID = window.setInterval(function() {goLines();}, 1);
-      var intervalIDTwo = window.setInterval(function() {goLinesTwo();}, 1);
-      document.getElementById("title").style.transitionDuration = "2s";
-      document.getElementById("title").style.color = "transparent";
-      var intervalIDThree = window.setInterval(function() {
-        document.getElementById("text").style.transitionDuration = "6s";
-        document.getElementById("text").style.color = "white";
-      }, 1800);
-    });
-}
 
-clickGo("click");
-clickGo("touchstart");
+  var textChange = function(element, time, color) {
+    document.getElementById(element).style.transitionDuration = time;
+    document.getElementById(element).style.color = color;
+  }
+
+
+  var clickGo = function(click) {
+    document.querySelector("body").addEventListener(click, function() {
+        window.setInterval(function() {goLines();}, 1);
+        window.setInterval(function() {goLinesTwo();}, 1);
+        textChange("title", "1s", "transparent")
+        window.setInterval(function() {textChange("text", "6s", "white");}, 1500);
+      });
+  }
+
+
+  clickGo("click");
+  clickGo("touchstart");
