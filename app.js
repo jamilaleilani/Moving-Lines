796bef83e0980035d4f57e9c5cd8875982a82daa
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-04-02 14:36:28 -0700

    made font sizing for responsive design

--- app.js
+++ app.js
@@ -8,55 +8,43 @@
 
 
   window.addEventListener("resize", function() {
-    document.getElementById("page").style.height = window.innerHeight + "px";
     document.getElementById("artboard").style.height = window.innerHeight + "px";
   })
 
   //add divs for boardWidth/10
 
-  for (var i = 0; i < boardHeight/20; i++) {
-    var newLine = document.createElement("div");
-    document.getElementById("artboard").style.height = boardHeight + "px";
-    newLine.classList.add("line");
-    document.getElementById("artboard").appendChild(newLine);
-    newLine.style.transitionDuration = ((Math.random() * 2.5) + .5).toString() + "s";
+  var makeLines = function(artboard, line) {
+    for (var i = 0; i < boardHeight/20; i++) {
+      var newLine = document.createElement("div");
+      document.getElementById(artboard).style.height = boardHeight + "px";
+      newLine.classList.add(line);
+      document.getElementById(artboard).appendChild(newLine);
+      newLine.style.transitionDuration = ((Math.random() * 2.5) + .5).toString() + "s";
+    }
   }
 
-  for (var i = 0; i < boardHeight/20; i++) {
-    var newLine = document.createElement("div");
-    newLine.classList.add("lineTwo");
-    document.getElementById("artboardTwo").appendChild(newLine);
-    newLine.style.transitionDuration = ((Math.random() * 2.5) + .5).toString() + "s";
-  }
+  makeLines("artboard", "line");
+  makeLines("artboardTwo", "lineTwo");
 
-  var goLines = function() {
-    document.querySelectorAll(".line").forEach(function(line) {
+  var goLines = function(lines) {
+    document.querySelectorAll(lines).forEach(function(line) {
       line.style.width = "10px";
     });
   }
-
-  var goLinesTwo = function() {
-    document.querySelectorAll(".lineTwo").forEach(function(line) {
-      line.style.width = "10px";
-    });
-  }
-
 
   var textChange = function(element, time, color) {
     document.getElementById(element).style.transitionDuration = time;
     document.getElementById(element).style.color = color;
   }
 
-
   var clickGo = function(click) {
     document.querySelector("body").addEventListener(click, function() {
-        window.setInterval(function() {goLines();}, 1);
-        window.setInterval(function() {goLinesTwo();}, 1);
+        window.setInterval(function() {goLines(".line");}, 1);
+        window.setInterval(function() {goLines(".lineTwo");}, 1);
         textChange("title", "1s", "transparent")
         window.setInterval(function() {textChange("text", "6s", "white");}, 1500);
       });
   }
-
 
   clickGo("click");
   clickGo("touchstart");
