fcea8d5dc71f94b9e3b32b44c3997a5eeb245ca4
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-04-02 14:46:49 -0700

    cleaned up code

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
   console.log("JS working??");
 
-  var boardWidth = document.getElementById("page").offsetWidth;
-  console.log("Board Width is: " + boardWidth);
   var boardHeight = document.getElementById("page").style.height = window.innerHeight;
-  console.log("boardHeight:", boardHeight);
-  console.log("innerHeight is: ",  window.innerHeight)
 
-
+  //have artboard resize with the window
   window.addEventListener("resize", function() {
     document.getElementById("artboard").style.height = window.innerHeight + "px";
+    document.getElementById("artboardTwo").style.height = window.innerHeight + "px";
   })
 
-  //add divs for boardWidth/10
-
+  //make lines, randomize the speed at which they shrink
   var makeLines = function(artboard, line) {
     for (var i = 0; i < boardHeight/20; i++) {
       var newLine = document.createElement("div");
@@ -26,17 +22,20 @@
   makeLines("artboard", "line");
   makeLines("artboardTwo", "lineTwo");
 
+
+  //have lines shrink to 10px width
   var goLines = function(lines) {
     document.querySelectorAll(lines).forEach(function(line) {
       line.style.width = "10px";
     });
   }
-
+  //function to have click disappear and text show up
   var textChange = function(element, time, color) {
     document.getElementById(element).style.transitionDuration = time;
     document.getElementById(element).style.color = color;
   }
 
+  //on click event, calls on above two funcitons
   var clickGo = function(click) {
     document.querySelector("body").addEventListener(click, function() {
         window.setInterval(function() {goLines(".line");}, 1);
@@ -46,5 +45,6 @@
       });
   }
 
+  //called on once for computer (click), and once for mobile (touchstart)
   clickGo("click");
   clickGo("touchstart");
